ada0443ee4d13e755a77e07a212f7c63b850b2ad
Chris Zuniga <zunigachristian81@gmail.com>
2021-10-18 15:38:24 -0500

    almost done with player object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,3 +1,5 @@
+const { test, expect } = require('@jest/globals');
+const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
@@ -13,4 +15,14 @@ test('creates a random potion object', () => {
   expect(potion.name).toEqual(expect.any(String));
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('Checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
 });
